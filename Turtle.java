afda33b9bf3f85fc6e4a52efbdc446b866231006
kdve <clplanet26@gmail.com>
2016-11-14 13:56:57 +0900

    second commit

--- Turtle.java
+++ Turtle.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Turtle extends Actor
+public class Turtle extends Lobster
 {
     /**
      * Act - do whatever the Turtle wants to do. This method is called whenever
--- Worm.java
+++ Worm.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Robster here.
+ * Write a description of class Worm here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Robster extends Actor
+public class Worm extends Actor
 {
     /**
-     * Act - do whatever the Robster wants to do. This method is called whenever
+     * Act - do whatever the Worm wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
