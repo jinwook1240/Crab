45a8e26da9d6d3563ed8f88e9a2f05922693f581
kdve <clplanet26@gmail.com>
2016-11-19 21:17:52 +0900

    수정중인 기능: Worm 랜덤 생성 기능

--- Crab.java
+++ Crab.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.awt.Color;
 
 /**
  * Write a description of class Crab here.
@@ -8,6 +9,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Crab extends Actor
 {
+    public int score=0;
+    private int speed=4;
+    GreenfootImage background;
+    public void addedToWorld(World world){
+        background = world.getBackground();
+    }
+
     /**
      * Act - do whatever the Crab wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +23,26 @@ public class Crab extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+        if(Greenfoot.isKeyDown("up")){
+            move(5);
+        }
+        if(Greenfoot.isKeyDown("down")){
+            move(-4);
+        }
+        if(Greenfoot.isKeyDown("left")){
+            turn(-5);
+        }
+        if(Greenfoot.isKeyDown("right")){
+            turn(5);
+        }
+        if(isTouching(Worm.class)){
+            removeTouching(Worm.class);
+            score += 10;
+            GreenfootImage scoreadd = new GreenfootImage(background);
+            scoreadd.setColor(Color.black);
+            scoreadd.drawString("점수 : "+score,20,20);
+            getWorld().setBackground(scoreadd);
+    }
+    if(isAtEdge())turn(Greenfoot.getRandomNumber(100)+130);
+}
 }
--- MyWorld.java
+++ MyWorld.java
@@ -8,14 +8,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
+    Worm[] worm = new Worm[8];
+    Crab crab = new Crab();
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 600, 1); 
+        addObject(crab, getWidth()/2,getHeight()/2);
+        for(int i=0;i<worm.length;i++){
+            worm[i] = new Worm();
+            addObject(worm[i],Greenfoot.getRandomNumber(getWidth()-10)+5,Greenfoot.getRandomNumber(getHeight()-10)+5);
+        }
+    }
+    public void act(){
+    }
+    private void ifNearcrab(Worm worm){
+        if(crab.
     }
 }
